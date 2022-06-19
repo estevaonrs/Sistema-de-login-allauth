7ba1f8f3d34f5e710331e744e557f8065a81412e
Estevao Ferreira Pequeno <tevonrs@gmail.com>
2022-06-19 16:40:26 -0300

    Pronto para deplo

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn allauth_introducao.wsgi
--- allauth_introducao/settings.py
+++ allauth_introducao/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -103,3 +105,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
